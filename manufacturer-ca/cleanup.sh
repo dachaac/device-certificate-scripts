14a962103db70a1e27e29cb5e50f57ba5299434d
Vesa Jääskeläinen <dachaac@gmail.com>
2019-11-15 22:06:54 +0200

    manufacturer-ca: Add cleanup script
    Signed-off-by: Vesa Jääskeläinen <dachaac@gmail.com>

---
+++ manufacturer-ca/cleanup.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+rm -rf	IOTDEVICE-K1234567.crt \
+	IOTDEVICE-K1234567.csr \
+	IOTDEVICE-K1234567.key \
+	IOTDEVICE-K1234567.key.password \
+	manufacturer-devices-ca \
+	manufacturer-root-ca
