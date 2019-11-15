e633529089e42ee44cb0ad018e0aa6616ae8d5b4
Vesa Jääskeläinen <dachaac@gmail.com>
2019-11-15 22:04:34 +0200

    service-ca: Add cleanup script
    Signed-off-by: Vesa Jääskeläinen <dachaac@gmail.com>

---
+++ service-ca/cleanup.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+rm -rf IOTDEVICE-K1234567.crt \
+	IOTDEVICE-K1234567.csr \
+	IOTDEVICE-K1234567.key \
+	IOTDEVICE-K1234567.key.password \
+	localhost.crt \
+	localhost.csr \
+	localhost.key \
+	localhost.key.password \
+	service-devices-ca \
+	service-root-ca \
+	service-servers-ca \
