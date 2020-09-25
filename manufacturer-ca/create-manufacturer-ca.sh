1d946d695ac011a4d2d8914398e9580e764bb33c
Vesa Jääskeläinen <dachaac@gmail.com>
2020-09-25 18:02:34 +0300

    Update hash algos based on Mozilla Root Store Policy
    Mozilla CA policy:
    https://www.mozilla.org/en-US/about/governance/policies/security-group/certs/policy/
    
    When a root or intermediate certificate's ECDSA key is used to produce a signature, only the following algorithms may be used, and with the following encoding requirements:
    
    If the signing key is P-256, the signature MUST use ECDSA with SHA-256. The encoded AlgorithmIdentifier MUST match the following hex-encoded bytes: 300a06082a8648ce3d040302.
    
    If the signing key is P-384, the signature MUST use ECDSA with SHA-384. The encoded AlgorithmIdentifier MUST match the following hex-encoded bytes: 300a06082a8648ce3d040303.
    
    Signed-off-by: Vesa Jääskeläinen <dachaac@gmail.com>

--- manufacturer-ca/create-manufacturer-ca.sh
+++ manufacturer-ca/create-manufacturer-ca.sh
@@ -3,7 +3,7 @@ echo "Creating Manufacturer's Products Root CA..."
 mkdir manufacturer-root-ca
 pwgen -s 24 1 > manufacturer-root-ca/manufacturer-root-ca.key.password
 openssl ecparam -name secp384r1 -genkey | openssl ec -passout file:manufacturer-root-ca/manufacturer-root-ca.key.password -aes256 -out manufacturer-root-ca/manufacturer-root-ca.key
-openssl req -new -config openssl.cnf -sha256 -nodes -subj "/C=FI/O=Manufacturer/CN=Products Root CA" -key manufacturer-root-ca/manufacturer-root-ca.key -passin file:manufacturer-root-ca/manufacturer-root-ca.key.password -out manufacturer-root-ca/manufacturer-root-ca.csr
+openssl req -new -config openssl.cnf -sha384 -nodes -subj "/C=FI/O=Manufacturer/CN=Products Root CA" -key manufacturer-root-ca/manufacturer-root-ca.key -passin file:manufacturer-root-ca/manufacturer-root-ca.key.password -out manufacturer-root-ca/manufacturer-root-ca.csr
 
 touch manufacturer-root-ca/index.txt
 echo 01 > manufacturer-root-ca/serial
@@ -15,7 +15,7 @@ echo "Creating Manufacturer's Devices CA..."
 mkdir manufacturer-devices-ca
 pwgen -s 24 1 > manufacturer-devices-ca/manufacturer-devices-ca.key.password
 openssl ecparam -name secp384r1 -genkey | openssl ec -passout file:manufacturer-devices-ca/manufacturer-devices-ca.key.password -aes256 -out manufacturer-devices-ca/manufacturer-devices-ca.key
-openssl req -new -config openssl.cnf -sha256 -nodes -subj "/C=FI/O=Manufacturer/CN=Devices CA" -key manufacturer-devices-ca/manufacturer-devices-ca.key -passin file:manufacturer-devices-ca/manufacturer-devices-ca.key.password -out manufacturer-devices-ca/manufacturer-devices-ca.csr
+openssl req -new -config openssl.cnf -sha384 -nodes -subj "/C=FI/O=Manufacturer/CN=Devices CA" -key manufacturer-devices-ca/manufacturer-devices-ca.key -passin file:manufacturer-devices-ca/manufacturer-devices-ca.key.password -out manufacturer-devices-ca/manufacturer-devices-ca.csr
 touch manufacturer-devices-ca/index.txt
 echo 01 > manufacturer-devices-ca/serial
 echo 01 > manufacturer-devices-ca/crlnumber
@@ -25,7 +25,7 @@ openssl ca -config openssl.cnf -name CA_root -extensions ext_intermediate -keyfi
 echo "Creating test device certificate..."
 pwgen -s 24 1 > IOTDEVICE-K1234567.key.password
 openssl ecparam -name prime256v1 -genkey | openssl ec -passout file:IOTDEVICE-K1234567.key.password -aes256 -out IOTDEVICE-K1234567.key
-openssl req -new -config openssl.cnf -sha256 -nodes -subj "/C=FI/O=Manufacturer/CN=IoT Device/serialNumber=K1234567" -key IOTDEVICE-K1234567.key -passin file:IOTDEVICE-K1234567.key.password -out IOTDEVICE-K1234567.csr
+openssl req -new -config openssl.cnf -sha384 -nodes -subj "/C=FI/O=Manufacturer/CN=IoT Device/serialNumber=K1234567" -key IOTDEVICE-K1234567.key -passin file:IOTDEVICE-K1234567.key.password -out IOTDEVICE-K1234567.csr
 
 openssl ca -config openssl.cnf -name CA_devices -extensions ext_client -keyfile manufacturer-devices-ca/manufacturer-devices-ca.key -passin file:manufacturer-devices-ca/manufacturer-devices-ca.key.password -cert manufacturer-devices-ca/manufacturer-devices-ca.crt -in IOTDEVICE-K1234567.csr -batch | openssl x509 -out IOTDEVICE-K1234567.crt -outform PEM
 
--- service-ca/create-service-ca.sh
+++ service-ca/create-service-ca.sh
@@ -3,7 +3,7 @@ echo "Creating Service Root CA..."
 mkdir service-root-ca
 pwgen -s 24 1 > service-root-ca/service-root-ca.key.password
 openssl ecparam -name secp384r1 -genkey | openssl ec -passout file:service-root-ca/service-root-ca.key.password -aes256 -out service-root-ca/service-root-ca.key
-openssl req -new -config openssl.cnf -sha256 -nodes -subj "/C=FI/O=Service/CN=Service Root CA" -key service-root-ca/service-root-ca.key -passin file:service-root-ca/service-root-ca.key.password -out service-root-ca/service-root-ca.csr
+openssl req -new -config openssl.cnf -sha384 -nodes -subj "/C=FI/O=Service/CN=Service Root CA" -key service-root-ca/service-root-ca.key -passin file:service-root-ca/service-root-ca.key.password -out service-root-ca/service-root-ca.csr
 
 touch service-root-ca/index.txt
 echo 01 > service-root-ca/serial
@@ -15,7 +15,7 @@ echo "Creating Service Devices CA..."
 mkdir service-devices-ca
 pwgen -s 24 1 > service-devices-ca/service-devices-ca.key.password
 openssl ecparam -name secp384r1 -genkey | openssl ec -passout file:service-devices-ca/service-devices-ca.key.password -aes256 -out service-devices-ca/service-devices-ca.key
-openssl req -new -config openssl.cnf -sha256 -nodes -subj "/C=FI/O=Service/CN=Devices CA" -key service-devices-ca/service-devices-ca.key -passin file:service-devices-ca/service-devices-ca.key.password -out service-devices-ca/service-devices-ca.csr
+openssl req -new -config openssl.cnf -sha384 -nodes -subj "/C=FI/O=Service/CN=Devices CA" -key service-devices-ca/service-devices-ca.key -passin file:service-devices-ca/service-devices-ca.key.password -out service-devices-ca/service-devices-ca.csr
 touch service-devices-ca/index.txt
 echo 01 > service-devices-ca/serial
 echo 01 > service-devices-ca/crlnumber
@@ -26,7 +26,7 @@ echo "Creating Service Servers CA..."
 mkdir service-servers-ca
 pwgen -s 24 1 > service-servers-ca/service-servers-ca.key.password
 openssl ecparam -name secp384r1 -genkey | openssl ec -passout file:service-servers-ca/service-servers-ca.key.password -aes256 -out service-servers-ca/service-servers-ca.key
-openssl req -new -config openssl.cnf -sha256 -nodes -days 23360 -subj "/C=FI/O=Service/CN=Servers CA" -key service-servers-ca/service-servers-ca.key -passin file:service-servers-ca/service-servers-ca.key.password -out service-servers-ca/service-servers-ca.csr
+openssl req -new -config openssl.cnf -sha384 -nodes -days 23360 -subj "/C=FI/O=Service/CN=Servers CA" -key service-servers-ca/service-servers-ca.key -passin file:service-servers-ca/service-servers-ca.key.password -out service-servers-ca/service-servers-ca.csr
 touch service-servers-ca/index.txt
 echo 01 > service-servers-ca/serial
 echo 01 > service-servers-ca/crlnumber
@@ -36,14 +36,14 @@ openssl ca -config openssl.cnf -name CA_root -extensions ext_intermediate -keyfi
 echo "Creating test device certificate..."
 pwgen -s 24 1 > IOTDEVICE-K1234567.key.password
 openssl ecparam -name prime256v1 -genkey | openssl ec -passout file:IOTDEVICE-K1234567.key.password -aes256 -out IOTDEVICE-K1234567.key
-openssl req -new -config openssl.cnf -sha256 -nodes -subj "/CN=urn:dev:ops:32473-IoT_Device-K1234567" -key IOTDEVICE-K1234567.key -passin file:IOTDEVICE-K1234567.key.password -out IOTDEVICE-K1234567.csr
+openssl req -new -config openssl.cnf -sha384 -nodes -subj "/CN=urn:dev:ops:32473-IoT_Device-K1234567" -key IOTDEVICE-K1234567.key -passin file:IOTDEVICE-K1234567.key.password -out IOTDEVICE-K1234567.csr
 
 openssl ca -config openssl.cnf -name CA_devices -extensions ext_client -keyfile service-devices-ca/service-devices-ca.key -passin file:service-devices-ca/service-devices-ca.key.password -cert service-devices-ca/service-devices-ca.crt -in IOTDEVICE-K1234567.csr -batch | openssl x509 -out IOTDEVICE-K1234567.crt -outform PEM
 
 echo "Creating localhost Server Certificate..."
 pwgen -s 24 1 > localhost.key.password
 openssl ecparam -name prime256v1 -genkey | openssl ec -passout file:localhost.key.password -aes256 -out localhost.key
-openssl req -new -config openssl.cnf -sha256 -nodes -days 23360 -subj "/C=FI/O=Service/CN=localhost" -addext "subjectAltName = DNS:localhost" -key localhost.key -passin file:localhost.key.password -out localhost.csr
+openssl req -new -config openssl.cnf -sha384 -nodes -days 23360 -subj "/C=FI/O=Service/CN=localhost" -addext "subjectAltName = DNS:localhost" -key localhost.key -passin file:localhost.key.password -out localhost.csr
 
 openssl ca -config openssl.cnf -name CA_servers -extensions ext_server -keyfile service-servers-ca/service-servers-ca.key -passin file:service-servers-ca/service-servers-ca.key.password -cert service-servers-ca/service-servers-ca.crt -in localhost.csr -batch | openssl x509 -out localhost.crt -outform PEM
 
